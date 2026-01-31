ec93eb740fc536adcc4b0d5d5a85c94f03452392
NaritaShosei <onigirikun7171@gmail.com>
2026-01-31 16:51:59 +0900

    feat:タイムマネージャーの作成

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private float _timeLimit = 200;
+
+    private GameState _state;
+    private TimeManager _timeManager;
+
+    private void Start()
+    {
+        _timeManager = new TimeManager(_timeLimit);
+
+        _timeManager.OnTimeEnd += OnTimeUp;
+    }
+
+    private void Update()
+    {
+        _timeManager.Tick(Time.deltaTime);
+    }
+
+    private void StartGame()
+    {
+        _state = GameState.Playing;
+    }
+
+    private void OnTimeUp()
+    {
+        FinishGame();
+    }
+
+    private void FinishGame()
+    {
+        _state = GameState.Finished;
+        Debug.Log("Game End");
+    }
+}
+
+public enum GameState
+{
+    [InspectorName("準備中")]
+    Ready,
+    [InspectorName("インゲーム")]
+    Playing,
+    [InspectorName("ゲーム終了")]
+    Finished,
+    [InspectorName("演出など再生中")]
+    Effect,
+}
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+public class TimeManager
+{
+    public event Action OnTimeEnd;
+    public event Action<float> OnTimeUpdate;
+
+    private float _timeLimit;
+    private float _currentTime;
+    private bool _isRunning;
+
+    public TimeManager(float time)
+    {
+        _timeLimit = time;
+        _currentTime = _timeLimit;
+    }
+
+    public void Start()
+    {
+        _isRunning = true;
+    }
+
+    public void Stop()
+    {
+        _isRunning = false;
+    }
+
+
+    public void Tick(float deltaTime)
+    {
+        if (!_isRunning) { return; }
+
+        _currentTime = Mathf.Max(0, _currentTime - deltaTime);
+        OnTimeUpdate?.Invoke(_currentTime);
+
+        if (_currentTime <= 0)
+        {
+            TimeEnd();
+        }
+    }
+
+
+    private void TimeEnd()
+    {
+        _isRunning = false;
+        OnTimeEnd?.Invoke();
+    }
+}
