459b7e2af459e95f4ee7178e8d5f777d8823f3dc
NaritaShosei <onigirikun7171@gmail.com>
2026-01-31 20:27:52 +0900

    feat:ステータス管理クラスの作成

---
+++ Assets/Scripts/Data/UnitStatsData.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "UnitStatsData", menuName = "GameData/UnitStatsData")]
+public class UnitStatsData : ScriptableObject
+{
+    /// <summary> 機動力（移動速度） </summary>
+    public int Mobility => _mobility;
+
+    /// <summary> 近距離攻撃力 </summary>
+    public int ShortRangeAttack => _shortRangeAttack;
+
+    /// <summary> 遠距離攻撃力 </summary>
+    public int LongRangeAttack => _longRangeAttack;
+
+    /// <summary> HP </summary>
+    public int MaxHP => _maxHP;
+
+    [SerializeField] private int _mobility = 100;
+    [SerializeField] private int _shortRangeAttack = 200;
+    [SerializeField] private int _longRangeAttack = 100;
+    [SerializeField] private int _maxHP = 100;
+}
---
+++ Assets/Scripts/InGame/Unit/UnitStats.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class UnitStats
+{
+    /// <summary> 機動力（移動速度） </summary>
+    public int Mobility => _mobility;
+
+    /// <summary> 近距離攻撃力 </summary>
+    public int ShortRangeAttack => _shortRangeAttack;
+
+    /// <summary> 遠距離攻撃力 </summary>
+    public int LongRangeAttack => _longRangeAttack;
+
+    /// <summary> HP </summary>
+    public int MaxHP => _maxHP;
+
+    public UnitStats(params UnitStatsData[] datas)
+    {
+        foreach (var data in datas)
+        {
+            _mobility += data.Mobility;
+            _shortRangeAttack += data.ShortRangeAttack;
+            _longRangeAttack += data.LongRangeAttack;
+            _maxHP += data.MaxHP;
+        }
+    }
+
+    private int _mobility;
+    private int _shortRangeAttack;
+    private int _longRangeAttack;
+    private int _maxHP;
+}
